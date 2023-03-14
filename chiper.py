460e816c378cab9dc2ab87fa7140be2b9916be7a
XBone-3 <124483349+XBone-3@users.noreply.github.com>
2023-03-14 21:38:52 +0530

    added arguments sys

--- chiper.py
+++ chiper.py
@@ -1,10 +1,18 @@
 import sys
 
+if len(sys.argv) < 3:
+    print("chiper.py takes 2 arguments 1.message 2.mode", "1. message to encrypt or decrypt", "2. mode 'e' for encryption 'd' for decryption", sep='\n')
+    sys.exit()
+
+if sys.argv[2] not in ('e', 'd'):
+    print("chiper takes e or d as mode arguments")
+    sys.exit()
+
 SYMBOLS = """ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-=`!@#$%^&*()_+~,./?;"':[{]}\\|"""
 
 message = sys.argv[1]
 
-key = 8
+key = 17
 
 mode = sys.argv[2]
 
@@ -25,4 +33,4 @@ for char in message:
     else:
         translated_message = translated_message + char
 
-print(translated_message)
+print(f'message after translation is "{translated_message}"')
